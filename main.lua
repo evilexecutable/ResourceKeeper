73b7857b8b78ccee6deec212c2861f828e33dc7e
evilexecutable <evilexecutable@gmail.com>
2016-04-07 10:06:04 +0100

    Initial commit - Base code

---
+++ main.lua
@@ -0,0 +1,25 @@
+-- Lets run a CMD command that will get the free space for the C drive and save to a file.
+os.execute('wmic /node:"%COMPUTERNAME%" LogicalDisk Where DriveType="3" Get DeviceID,FreeSpace|find /I "c:" > freespace.txt')
+-- Now lets make that file the only input file.
+loadfreespace = assert(io.input("freespace.txt"))
+-- Convert from userdata to string and read the entire file (it's only 1 line)
+tostring(loadfreespace)
+readfreespace = io.read("*all")
+-- Print it to test it has only displayed the free space. Now we can convert from bytes to GB.
+--test = print(tonumber(string.match(readfreespace, "%d+")))
+-- Calculate from B to GB and print
+getspace = tonumber(string.match(readfreespace, "%d+")) / 1024 / 1024 / 1024
+print(getspace .. " Gigabytes Free on C:\\")
+-- Close the freespace file so we can continue to open some later
+io.input():close()
+-------------------------------------------------------------------------------------------------------
+
+-- Now lets calculate RAM in the same way.
+os.execute('wmic OS get FreePhysicalMemory > freeram.txt')
+-- Load file again.
+loadfreeram = assert(io.input("freeram.txt"))
+-- and we convert to string
+tostring(loadfreeram)
+readfreeram = io.read("*all")
+-- Test print
+test2 = print(tonumber(string.match(readfreeram, "%d+")))
